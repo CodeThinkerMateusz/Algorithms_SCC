7295bca026875c0e0b088cdd464b78ccfecf5e3c
Mateusz <01199214@pw.edu.pl>
2026-01-22 13:42:01 +0100

    assign, DFS, transposed

--- SCC/main.cpp
+++ SCC/main.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+
 
 using namespace std;
 
 
-void assign(int n, int m) {
-	vector<vector<int>>graph(n);
 
+void DFS(vector<vector<int>>& graph, int u, vector<bool> &checked, stack<int> &mystack) {
+	if (checked[u])return;
+	checked[u] = true;
+	for (int i = 0; i < graph[u].size(); i++) {
+		if (!checked[graph[u][i]]) {
+			checked[graph[u][i]] = true;
+			DFS(graph, graph[u][i], checked, mystack);
+		}
+	}
+	mystack.push(u);
+}
+
+//creating  graph
+void assign(vector<vector<int>>&graph , int n, int m) {
 	for (int i = 0; i < m; i++) {
+		int from, to;
+		cin >> from >> to;
 
+		graph[from].push_back(to);
 	}
 }
+
+void make_Transposed(vector<vector<int>>&graphT, vector<vector<int>>&graph, int n) {
+	for (int u = 0; u < n; u++) {
+		for (int v : graph[u]) {
+			graphT[v].push_back(u);
+		}
+	}
+}
+
+
 
 int main() {
 	int z;
@@ -20,8 +47,18 @@ int main() {
 		int n, m;
 		cin >> n >> m;
 		if (n <= 0 || m <= 0)return 1;
-
-		assign(n, m);
+		vector<vector<int>>graph(n);
+		stack<int> mystack;
+		vector<bool> checked(n, false);
+		vector<vector<int>>graphT(n);
+		assign(graph, n, m);
+		
+		for (int u = 0; u < n; u++) {
+			DFS(graph, u, checked, mystack);
+		}
+		//nie wiem  czy sprawdzenie czy cala  tablica  jest  checked  jest  potrzbene
+		make_Transposed(graph, graphT, n);
+		
 	}
 
 	return 0;
